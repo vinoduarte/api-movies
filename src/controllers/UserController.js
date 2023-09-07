225ec06c79ad466db649b78193182766b9cb094b
Vinícius <vinoduarte11@gmail.com>
2023-09-06 23:16:48 -0300

    little changes and testing git

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name) {
+      return AppError("Você não colocou o nome", 102)
+    }
 
     response.json({ name, email, password })
   }
