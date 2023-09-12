9a2d90f93e2e452edf6271ebf8eac8ed03ad12ea
Vinícius <vinoduarte11@gmail.com>
2023-09-11 21:10:36 -0300

    password update, bug null about to be fixed

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,7 @@
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 const { hash } = require("bcryptjs")
+const { compare } = require("bcryptjs")
 
 class UserController {
   async create(request, response) {
@@ -22,7 +23,7 @@ class UserController {
   }
 
   async update(request, response) {
-    const { name, email, password } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
     
     const database = await sqliteConnection()
@@ -38,10 +39,24 @@ class UserController {
       throw new AppError("Este email já está em uso")
     }
 
+    if(password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para defirnir a nova senha")
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     user.name = name
     user.email = email
 
-    await database.run(`UPDATE users SET name = ?, email = ?, updated_at = ? WHERE id = ?`, [user.name, user.email, new Date(), id])
+    await database.run(`UPDATE users SET name = ?, email = ?, password = ?, updated_at = ? WHERE id = ?`, [user.name, user.email, user.password, new Date(), id])
 
     return response.json()
   }
